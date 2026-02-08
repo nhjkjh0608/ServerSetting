a1787fa4263048398d1354cebff6fbcee98bfd66
nhjkjh0608 <nhjkjh0608@gmail.com>
2026-02-09 02:12:49 +0900

    minor bug fixed

--- 20_enable_ubuntu_pro_subscription.sh
+++ 20_enable_ubuntu_pro_subscription.sh
@@ -9,6 +9,11 @@ fi
 
 # enable ubuntu pro subscription
 read -r -s -p "Enter your token: " UBUNTU_PRO_TOKEN
+
+if ! pro attach "$UBUNTU_PRO_TOKEN"; then
+  exit 1
+fi
+
 pro attach "$UBUNTU_PRO_TOKEN" || true
 pro enable esm-apps  || true
 pro enable esm-infra || true
